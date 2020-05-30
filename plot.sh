5b16e8faa087a7f3afa51e80e7c9cf8057da9eca
Piotr (Peter) Slatala <psla-github@sepio.pl>
2020-05-29 21:05:45 -0700

    Draw just lines

--- plot.sh
+++ plot.sh
@@ -7,10 +7,10 @@ gnuplot <<-EOFMarker
     set xlabel "Time"
     set ylabel "MB/s"
     #set xdata time
-    set pointsize 1
+    set pointsize 0
     set yrange [0:220]
     set y2range [0:60]
-    plot "~tmp" using 1:2  title "read" , \
-         "~tmp" using 1:3  title "write", \
-         "~tmp" using 1:4  title "temp" axes x1y2
+    plot "~tmp" using 1:2 with lines title "read" , \
+         "~tmp" using 1:3 with lines title "write", \
+         "~tmp" using 1:4 with lines title "temp" axes x1y2
 EOFMarker
