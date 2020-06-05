2465bb529895fb9563617baccaa6e52dc79bed6d
Piotr (Peter) Slatala <psla-github@sepio.pl>
2020-06-05 10:23:15 -0700

    logs from sdc

--- stats.sh
+++ stats.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-drive=sdg
+drive=sdc
 fdisk -l /dev/$drive
 while [ true ]
 do
   # note that the date here is approximate: smartctl and vmstat have variance in how long they take. So over time, the average
   # is going to be correct -- but for a single data point this is just an approximation!
-  echo `date +%s%N` `smartctl -a /dev/$drive | grep -i temperat` `vmstat -d | grep $drive`
+  echo `date +%s%N` `smartctl -a /dev/$drive -d sat | grep -i temperat` `vmstat -d | grep $drive`
   sleep 5
 done
